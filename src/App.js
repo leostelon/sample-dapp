df9edcdcb95deb92bc660695803eca80ad6d7d1a
Leo Stelon <53221136+leostelon@users.noreply.github.com>
2024-08-06 22:44:54 +0530

    basic wallet connect

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import { connectWalletToSite, getWalletAddress } from "./utils/wallet";
 
 function App() {
+	const [walletAddress, setWalletAddress] = useState();
+
+	async function connect() {
+		const connected = await connectWalletToSite();
+		if (connected) {
+			const address = await getWalletAddress();
+			setWalletAddress(address);
+		}
+	}
+
+	useEffect(() => {
+		connect();
+	}, []);
+
 	return (
 		<div className="App">
-			<header className="App-header">Hello World</header>
+			<header className="App-header">
+				{walletAddress ?? "Wallet Not Connected"}
+			</header>
 		</div>
 	);
 }
